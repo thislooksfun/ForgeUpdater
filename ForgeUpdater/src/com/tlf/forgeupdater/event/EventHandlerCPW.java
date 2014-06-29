a8f7bcc8082600094afe8db50e3131914252c837
thislooksfun <thislooksfun@repbot.org>
2014-06-29 14:58:51 -0500

    Compacted the code and tweaked the hover text

--- ForgeUpdater/src/com/tlf/forgeupdater/event/EventHandlerCPW.java
+++ ForgeUpdater/src/com/tlf/forgeupdater/event/EventHandlerCPW.java
@@ -6,6 +6,7 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.ChatStyle;
 import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.IChatComponent;
 
 import com.tlf.forgeupdater.checker.UpdateCheckManager;
 
@@ -32,11 +33,9 @@ public class EventHandlerCPW
 				String msg = "There "+(updates == 1 ? "is " : "are ")+EnumChatFormatting.RED+updates+EnumChatFormatting.AQUA+" mod"+(updates == 1 ? "" : "s")+" with"+(updates == 1 ? " a" : "")+" new version"+(updates == 1 ? "" : "s")+".";
 				event.player.addChatMessage(new ChatComponentText(msg).setChatStyle(new ChatStyle().setColor(EnumChatFormatting.AQUA)));
 				
-				ChatComponentText chatComponentBody = new ChatComponentText("For more information, please type ");
-				ChatComponentText chatComponentLink = new ChatComponentText("/updates");
-				chatComponentLink.setChatStyle(new ChatStyle().setColor(EnumChatFormatting.RED).setChatClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/updates")).setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ChatComponentText("/updates").setChatStyle(new ChatStyle().setColor(EnumChatFormatting.RED)))));
-				chatComponentBody.setChatStyle(new ChatStyle().setColor(EnumChatFormatting.AQUA)).appendSibling(chatComponentLink);
-				event.player.addChatMessage(chatComponentBody);
+				IChatComponent chatComponentBody = new ChatComponentText("For more information, please type ").setChatStyle(new ChatStyle().setColor(EnumChatFormatting.AQUA));
+				IChatComponent chatComponentLink = new ChatComponentText("/updates").setChatStyle(new ChatStyle().setColor(EnumChatFormatting.RED).setChatClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/updates")).setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ChatComponentText("Run command").setChatStyle(new ChatStyle().setColor(EnumChatFormatting.GOLD)))));;
+				event.player.addChatMessage(chatComponentBody.appendSibling(chatComponentLink));
 			}
 		}
 	}
